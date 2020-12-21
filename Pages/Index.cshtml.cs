e18310127c8027db621c34dab099594c2ed58eb1
Max-Gabriel-Susman <gabe.susman@gmail.com>
2020-12-20 20:54:50 -0700

    Implemented a pagemodel for the index

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -11,15 +11,17 @@ namespace myWebApp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
+	
+	public string Heading { get; set; } 
+	
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
-
+	
         public void OnGet()
         {
-
+	    Heading = "Straight out of the page model ol' son";
         }
     }
 }
